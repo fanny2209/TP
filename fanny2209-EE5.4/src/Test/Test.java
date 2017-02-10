35b8fb4403d58a35d0767f25c2e5e63fca26fbf9
fanny2209 <fanny.jammes@hotmail.fr>
2017-02-10 16:41:58 +0100

    maj foncion main

--- fanny2209-EE5.4/src/Test/Test.java
+++ fanny2209-EE5.4/src/Test/Test.java
@@ -5,31 +5,32 @@ import DeckUtils.Deck;
 
 public class Test {
 
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BlackjackHand blackhand = new BlackjackHand();
 		Deck deck = new Deck(false);
-		int randomHand = (int) ((Math.random() * (5)) + 2);
-		deck.shuffle(); 
-		blackhand=DealRandomCards(blackhand, randomHand, deck);
+		blackhand=DealRandomCards(blackhand, deck);
 	}
-	
+
+
+
 	/**
 	 *  Take random cards of the deck and give x cards to the blackjack hand
 	 * @param b take a black jack hand in parameter to adress cards
-	 * @param random : number of card deal
 	 * @param d: deck of the game 
 	 * @return a blackjackhand with initalize cards 
 	 * 
 	 */
-	public static BlackjackHand DealRandomCards(BlackjackHand b, int random, Deck d){
-		for(int i=0; i<random; i++){ 
+	public static BlackjackHand DealRandomCards(BlackjackHand b,Deck d){
+		int randomHand = (int) ((Math.random() * (5)) + 2);
+		d.shuffle(); 
+		for(int i=0; i<randomHand; i++){ 
 			Card c= d.dealCard();
 			b.addCard(c);
 		}
 		System.out.println("Gamer has this cards : ");
-		for(int i=0; i<random; i++){
+		for(int i=0; i<randomHand; i++){
 			System.out.println(b.getCard(i));
 		}
 		return b;
